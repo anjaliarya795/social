40535a60a9fd17fee2cd2c8564c5b840851d10f3
Anjali Arya <anjalisinha498@gmail.com>
2023-07-04 23:43:40 +0530

    sanity-setup

---
+++ sanity-backend/schemas/comment.ts
@@ -0,0 +1,18 @@
+export default {
+    name: 'comment',
+    title: 'Comment',
+    type: 'document',
+    fields: [
+        {
+            name: 'postedBy',
+            title: 'Posted By',
+            type: 'postedby',
+        },
+
+        {
+            name: 'comment',
+            title: 'Comment',
+            type: 'string',
+        }
+    ]
+}
